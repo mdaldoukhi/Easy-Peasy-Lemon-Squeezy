9825c14bee6b6ea47361f3352bbafaa253115719
SalwaBaqer <s.baqer@hotmail.com>
2021-02-08 15:57:24 +0300

    CRUD

---
+++ challenge.js
@@ -0,0 +1,42 @@
+//Welcome to ⭐️ B&H libarary ⭐️
+const library = [
+  { name: "The Hollow", author: "Agatha Christie", price: 3 },
+  { name: "The Book Thief", author: "Markus Zusak", price: 5 },
+  { name: "The ABC Murders", author: "Agatha Christie", price: 2.5 },
+  { name: "And Then There Were None", author: "Agatha Christie", price: 3.5 },
+  { name: "The Queen's Gambit", author: "Walter Tevis", price: 4 },
+  {
+    name: "Harry Potter and the Deathly Hallows",
+    author: "J. K. Rowling ",
+    price: 3,
+  },
+  {
+    name: "Harry Potter and the Order of the Phoenix",
+    author: "J. K. Rowling ",
+    price: 3,
+  },
+];
+
+const users = [
+  /** add your code here
+   array of users 😈
+   */
+];
+
+/**
+ ============================Part 1 ============================
+1.The user should be able to search for books by title , author ,and prices 🤑
+2.The user should be able to add a book to his wishlist
+3.The user should be able to add a book to the  cart
+4.The user should be able to see the items in the cart and the total amount 💸💰 
+
+ ============================Part 2 ============================
+ 1.The admin should be able to view all the items
+ 2.The admin should be able to add new items to the library
+ 3.The admin should be able to edit items
+ 4.The admin should be able to remove an item
+
+ Good luck 😍
+*/
+
+// your code here
